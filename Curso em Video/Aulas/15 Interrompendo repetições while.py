f1c632090b09134588c648d298cc591e7de76b69
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-07-27 21:32:23 +0100

    Updated: desafio066.py → desafio071.py 15 Interrompendo repetições while.py
    Signed-off-by: KillPC <21142629+DavidPPMerino@users.noreply.github.com>

---
+++ Curso em Video/Aulas/15 Interrompendo repetições while.py
@@ -0,0 +1,15 @@
+""" Laços de Repetição (parte 3) """
+
+""" Interromper laço while """
+
+while True: # <--- loop infinito
+    if a:
+        passo
+    if b:
+        pula
+    if c:
+        pega
+    if trofeu:
+        pula
+        break # <--- comando para interromper
+pega
---
+++ Curso em Video/PythonTeste/aula15a.py
@@ -0,0 +1,28 @@
+# cont = 1
+# while cont <= 10:
+#     print(f'{cont} → ', end='')
+#     cont += 1
+# print('Acabou')
+
+""" Repetição com flags """
+# n = 0
+# while n != 999:
+#     n = int(input('Digite um número: '))
+#
+# n = s = 0
+# while True:
+#     n = int(input('Digite um número: '))
+#     if n == 999:
+#         break
+#     s += n
+# print(f'A soma vale {s}')
+
+""" PEP == Python Enhancement Proposal 498 """
+# dado a conhecer
+"F Strings"
+
+# {nome:20} == "nome                 blablabla"
+# {nome:^20} == "        nome        "
+# {nome:-^20} == "--------nome--------"
+# {nome:->20} == "----------------nome"
+# {nome:-<20} == "nome----------------"
