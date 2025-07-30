a5c3c9cd514ea1b20f628e4b4969e5c77ca6dd76
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-07-30 18:51:15 +0100

    Updated desafio071.py description

--- Curso em Video/Desafios/7x/desafio071.py
+++ Curso em Video/Desafios/7x/desafio071.py
@@ -1,5 +1,8 @@
 """ Enunciado:
-        Crie um programa de um caixa
+        Crie um programa que simule o funcionamento de um 'caixa eletrônico'.
+        No início, pergunte ao usuário qual será o 'valor a ser sacado'
+        (número inteiro) e o programa vai informar quantas 'cédulas' de cada
+        valor serão entregues.
 """
 
 " V1.0 "
