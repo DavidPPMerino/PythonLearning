5f05a8073697403b711f6b365cf6ebfb4e0edd8a
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-07-31 16:06:28 +0100

    Solved desafio071.py

--- Curso em Video/Desafios/7x/desafio071.py
+++ Curso em Video/Desafios/7x/desafio071.py
@@ -6,5 +6,32 @@
 
         OBS: Considere que o caixa possui cédulas de 'R$50', 'R$20', 'R$10' e 'R$1.'
 """
-123
+
 " V1.0 "
+print('-=' * 20 + '-')
+print(f'{'Caixa Eletrônico':^41}')
+print('-=' * 20 + '-')
+amount = int(input('Que valor quer levantar? R$'))
+
+note50 = amount // 50
+amount = amount % 50
+note20 = amount // 20
+amount = amount % 20
+note10 = amount // 10
+amount = amount % 10
+note5 = amount // 5
+amount = amount % 5
+note1 = amount // 1
+print('-=' * 20 + '-')
+if note50 != 0:
+        print(f'Total de {note50} cédulas de R$50')
+if note20 != 0:
+        print(f'Total de {note20} cédulas de R$20')
+if note10 != 0:
+        print(f'Total de {note10} cédulas de R$10')
+if note5 != 0:        
+        print(f'Total de {note5} cédulas de R$5')
+if note1 != 0:
+        print(f'Total de {note1} cédulas de R$1')
+print('-=' * 20 + '-')
+print('Volte sempre. Tenha um bom dia!')
