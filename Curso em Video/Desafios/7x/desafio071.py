4da24982516234eb20789d869c9fbcf8cb6a4722
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-07-31 18:18:07 +0100

    Refactor indentation on desafio071.py

--- Curso em Video/Desafios/7x/desafio071.py
+++ Curso em Video/Desafios/7x/desafio071.py
@@ -22,13 +22,13 @@
 # note1 = amount // 1
 # print('-=' * 20 + '-')
 # if note50 != 0:
-#         print(f'Total de {note50} cédulas de R$50')
+#     print(f'Total de {note50} cédulas de R$50')
 # if note20 != 0:
-#         print(f'Total de {note20} cédulas de R$20')
+#     print(f'Total de {note20} cédulas de R$20')
 # if note10 != 0:
-#         print(f'Total de {note10} cédulas de R$10')
+#     print(f'Total de {note10} cédulas de R$10')
 # if note1 != 0:
-#         print(f'Total de {note1} cédulas de R$1')
+#     print(f'Total de {note1} cédulas de R$1')
 # print('-=' * 20 + '-')
 # print('Volte sempre. Tenha um bom dia!')
 
@@ -41,20 +41,20 @@ total = valor
 céd = 50
 totcéd = 0
 while True:
-        if total >= céd:
-                total -= céd
-                totcéd += 1
-        else:
-                if totcéd > 0:
-                        print(f'Total de {totcéd} cédulas de R${céd}')
-                if céd == 50:
-                        céd = 20
-                elif céd == 20:
-                        céd = 10
-                elif céd == 10:
-                        céd = 1
-                totcéd = 0
-        if total == 0:
-                break
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+                print(f'Total de {totcéd} cédulas de R${céd}')
+        if céd == 50:
+                céd = 20
+        elif céd == 20:
+                céd = 10
+        elif céd == 10:
+                céd = 1
+        totcéd = 0
+    if total == 0:
+            break
 print('=' * 30)
 print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
