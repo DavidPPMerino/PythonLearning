5b21575b8305e87c1cb9f54e08207c6b7f6476e0
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-02 00:37:28 +0100

    Solved: desafio073.py

--- Curso em Video/Desafios/7x/desafio073.py
+++ Curso em Video/Desafios/7x/desafio073.py
@@ -1,7 +1,22 @@
 """ Enunciado:
-Crie uma 'tupla' preenchida com os '20 primeiros' colocados da Tabela do 'Campeonato Brasileiro de Futebol', na ordem de colocação. Depois mostre:
+Crie uma 'tupla' preenchida com os '20 primeiros' colocados da Tabela do 
+'Campeonato Brasileiro de Futebol', na ordem de colocação. Depois mostre:
 a) Apenas os '5 primeiros' colocados.
 b) Os 'últimos 4' colocados da tabela.
 c) Uma lista com os times em 'ordem alfabética'.
 d) Em que 'posição' na tabela está o time da 'Chapecoense'
 """
+times = ("Coritiba", "Goiás", "Novorizontino", "Chapecoense", "Remo", "Criciúma", "Cuiabá", "Avaí", "Vila Nova", "CRB", "Athletico-PR", "Athletic Club", "Operário-PR", "Atlético-GO", "Ferroviária", "América-MG", "Volta Redonda", "Paysandu", "Amazonas", "Botafogo-SP")
+barra = ('-' * 20 + '-')
+print(barra)
+print(f'Lista de times: {times}')
+print(barra)
+print(f"Os primeiros 5 colocados: {times[0:5]}")
+print(barra)
+print(f"Os 4 ultimos são {times[-4:]}")
+print(barra)
+print(f"Times em ordem algabética: {sorted(times)}")
+print(barra)
+print(f"O Chapecoense está na {times.index('Chapecoense') + 1}ª posição")
+print(barra)
+print("Fim do programa")
