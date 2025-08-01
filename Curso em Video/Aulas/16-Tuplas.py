6821844a112a924ec0b695abd979715ae68330af
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-01 16:02:00 +0100

    Created: 16-Tuplas.py

---
+++ Curso em Video/Aulas/16-Tuplas.py
@@ -0,0 +1,28 @@
+""" Variáveis Compostas (Tuplas) """
+
+variável simples
+
+lanche = hamburguer
+lanche = suco
+apaga o hamburguer
+
+variável composta
+tupla
+
+lanche = hamburguer, suco, pizza, pudim
+            0        1     2      3
+parecido ao fatiamento de string
+
+print(lanche[2]) == pizza
+print(lanche[0:2]) == hamburguer, suco
+print(lanche[1:]) == suco pizza pudim
+print(lanche[-1]) == pudim
+print(lanche[-3]) == suco
+len(lanche) == 4
+for c in lanche:
+    print(c)
+cada repetição substitui o c por outro item
+
+AS TUPLAS SÃO IMUTÁVEIS!!!
+
+
