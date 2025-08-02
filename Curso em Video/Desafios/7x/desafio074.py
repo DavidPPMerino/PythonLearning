70d6461c2514a05f25e033f30e3a99b46a16bf68
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-02 17:27:40 +0100

    Created and Solved: desafio074.py

---
+++ Curso em Video/Desafios/7x/desafio074.py
@@ -0,0 +1,32 @@
+""" Enunciado:
+Crie um programa que vai gerar 'cinco números aleatórios e colocar em uma 
+'tupla'.
+Depois disso, mostra a 'listagem de números' gerados e tambem indique 
+o 'menor' e o 'maior' valor que estão na tupla.
+"""
+from random import randint
+
+n1 = randint(0, 10)
+n2 = randint(0, 10)
+n3 = randint(0, 10)
+n4 = randint(0, 10)
+n5 = randint(0, 10)
+nums = (n1, n2, n3, n4, n5)
+high = 0
+low = 0
+print("Os valores sorteados foram: ", end='')
+for i, num in enumerate(nums):
+    if i == 4:
+        print(num)
+    else:
+        print(num, end=' ')
+    if i == 0:
+        high = num
+        low = num
+    elif num > high:
+        high = num
+    elif num < low:
+        low = num
+# print(i)
+print(f"O maior valor sorteado foi {high}")
+print(f"O menor valor sorteado foi {low}")
