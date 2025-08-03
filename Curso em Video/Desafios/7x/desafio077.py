1fd92c1c1c91769dba0aecf81c26836dd18b3834
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-03 01:10:12 +0100

    Created: desafio077.py

---
+++ Curso em Video/Desafios/7x/desafio077.py
@@ -0,0 +1,4 @@
+""" Enunciado:
+Crie um programa que tenha uma 'tupla' com 'várias palavras' (não usar acentos).
+Depois disso, você deve mostrar, para cada palavra, quais são as suas 'vogais'.
+"""
