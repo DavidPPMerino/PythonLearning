70c502c8b8baa2fcc3aacc7e0c24dcc3d7ab52ef
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-04 13:29:26 +0100

    Solved: desafio077.py

--- Curso em Video/Desafios/7x/desafio077.py
+++ Curso em Video/Desafios/7x/desafio077.py
@@ -2,3 +2,11 @@
 Crie um programa que tenha uma 'tupla' com 'várias palavras' (não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas 'vogais'.
 """
+words = "aprender", "programar", "linguagem", "python", "curso", "gratis", "estudar", "praticar", "trabalhar", "mercado", "programador", "futuro"
+vowels = "a", "e", "i", "o", "u"
+for word in words:
+    print(f"Na palavra {word.upper()} temos ", end='')
+    for letter in word:
+        if letter in vowels:
+            print(f"{letter} ", end='')
+    print()
