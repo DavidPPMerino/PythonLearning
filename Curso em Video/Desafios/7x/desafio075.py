f92ebb595c5740d103add77ba7fb5b115a37f6a0
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-04 15:18:12 +0100

    Added Curso em Video solution: desafio075.py

--- Curso em Video/Desafios/7x/desafio075.py
+++ Curso em Video/Desafios/7x/desafio075.py
@@ -23,3 +23,19 @@ for i, num in enumerate(t):
                 print(", ",num, end='')
     else:
         print("nenhum número par encontrado.")
+
+""" Resolução Curso em Video """
+núm = (int(input("Escreva um número: ")),
+       int(input("Escreva outro número: ")),
+       int(input("Escreva mais um número: ")),
+       int(input("Escreva um ultimo número: ")))
+print(f'Você digitou os valores {num}')
+print(f'O valor 9 apareceu {núm.count(9)} vezes')
+if 3 in núm:
+    print(f'O valor 3 apareceu na {núm.index(3)+1}ª posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+print('Os valores pares digitados foram ', end='')
+for n in núm:
+    if n % 2 == 0:
+        print(n, end=' ')
