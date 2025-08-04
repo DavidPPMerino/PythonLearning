9fd2387c173287668b12e063a5e8115e057d9843
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-04 15:34:02 +0100

    Added Curso em Video solution: desafio076.py

--- Curso em Video/Desafios/7x/desafio076.py
+++ Curso em Video/Desafios/7x/desafio076.py
@@ -3,7 +3,9 @@ Crie um programa que tenha uma 'tupla' única com 'nomes de produtos' e seus
 respetivos 'preços', na sequência. 
 No final, mostre uma listagem de preços, organizando os dados em forma 'tabular'.
 """
-product_list = "Lápis", 1.75, "Borracha", 2.00, "Caderno", 15.90, "Estojo", 25.00, "Transferidor", 4.20, "Compasso", 9.99, "Mochila", 120.32, "Canetas", 22.30, "Livro", 34.90
+product_list = ("Lápis", 1.75, "Borracha", 2.00, "Caderno", 15.90, 
+                "Estojo", 25.00, "Transferidor", 4.20, "Compasso", 9.99, 
+                "Mochila", 120.32, "Canetas", 22.30, "Livro", 34.90)
 print("-" * 40)
 print(f"LISTAGEM DE PREÇOS".center(40))
 print("-" * 40)
@@ -13,3 +15,17 @@ for i, list in enumerate(product_list):
     else:
         print(f"{list:>6.2f}")
 print("-" * 40)        
+
+""" Resolução Curso em Video """
+listagem = ("Lápis", 1.75, "Borracha", 2.00, "Caderno", 15.90, 
+            "Estojo", 25.00, "Transferidor", 4.20, "Compasso", 9.99, 
+            "Mochila", 120.32, "Canetas", 22.30, "Livro", 34.90)
+print('-' * 40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-' * 40)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-' * 40)        
