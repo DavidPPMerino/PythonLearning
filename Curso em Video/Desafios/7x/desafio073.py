05fb3bf97be763301c77e71c4834d71568b381f0
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-04 14:19:23 +0100

    Added Curso em Video Solution: desafio073.py

--- Curso em Video/Desafios/7x/desafio073.py
+++ Curso em Video/Desafios/7x/desafio073.py
@@ -6,7 +6,10 @@ b) Os 'últimos 4' colocados da tabela.
 c) Uma lista com os times em 'ordem alfabética'.
 d) Em que 'posição' na tabela está o time da 'Chapecoense'
 """
-times = ("Coritiba", "Goiás", "Novorizontino", "Chapecoense", "Remo", "Criciúma", "Cuiabá", "Avaí", "Vila Nova", "CRB", "Athletico-PR", "Athletic Club", "Operário-PR", "Atlético-GO", "Ferroviária", "América-MG", "Volta Redonda", "Paysandu", "Amazonas", "Botafogo-SP")
+times = ("Coritiba", "Goiás", "Novorizontino", "Chapecoense", "Remo", 
+         "Criciúma", "Cuiabá", "Avaí", "Vila Nova", "CRB", "Athletico-PR", 
+         "Athletic Club", "Operário-PR", "Atlético-GO", "Ferroviária", 
+         "América-MG", "Volta Redonda", "Paysandu", "Amazonas", "Botafogo-SP")
 barra = ('-' * 20 + '-')
 print(barra)
 print(f'Lista de times: {times}')
@@ -15,8 +18,24 @@ print(f"Os primeiros 5 colocados: {times[0:5]}")
 print(barra)
 print(f"Os 4 ultimos são {times[-4:]}")
 print(barra)
-print(f"Times em ordem algabética: {sorted(times)}")
+print(f"Times em ordem alfabética: {sorted(times)}")
 print(barra)
 print(f"O Chapecoense está na {times.index('Chapecoense') + 1}ª posição")
 print(barra)
 print("Fim do programa")
+
+""" Resolução Curso em Video """
+times = ("Coritiba", "Goiás", "Novorizontino", "Chapecoense", "Remo", 
+         "Criciúma", "Cuiabá", "Avaí", "Vila Nova", "CRB", "Athletico-PR", 
+         "Athletic Club", "Operário-PR", "Atlético-GO", "Ferroviária", 
+         "América-MG", "Volta Redonda", "Paysandu", "Amazonas", "Botafogo-SP")
+print('-=' * 15)
+print(f'Lista de times do Brasileirão: {times}')
+print('-=' * 15)
+print(f"Os 5 primeiros são {times[0:5]}")
+print('-=' * 15)
+print(f"Os 4 ultimos são {times[-4:]}")
+print('-=' * 15)
+print(f"Times em ordem alfabética: {sorted(times)}")
+print('-=' * 15)
+print(f"O Chapecoense está na {times.index('Chapecoense')+1}ª posição")
