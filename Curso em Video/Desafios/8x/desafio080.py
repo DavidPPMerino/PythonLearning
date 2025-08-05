390c00751ce70b0d805a0033c42acd081ec49435
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-06 00:59:28 +0100

    Changed: var list→nums

--- Curso em Video/Desafios/8x/desafio080.py
+++ Curso em Video/Desafios/8x/desafio080.py
@@ -3,18 +3,17 @@ Crie um programa onde o usuário possa digitar 'cinco valores numéricos' e
 cadastre-os em uma 'lista', 'já na posição correta' de inserção (sem usar o 
 'sort()').
 No final, mostre a 'lista ordenada' na tela."""
-list = []
-# print(list[-2])
+nums = []
 for _ in range(5):
     new = int(input("Digite um valor: "))
-    if len(list) == 0:
-        list.append(new)
+    if len(nums) == 0:
+        nums.append(new)
     else:
-        for loc, num in enumerate(list):
+        for loc, num in enumerate(nums):
             if new < num:
-                list.insert(loc, new)
+                nums.insert(loc, new)
                 break
             else:
-                list.append(new)
+                nums.append(new)
                 break
-print(list)
+print(nums)
---
+++ Curso em Video/Desafios/8x/desafio081.py
@@ -0,0 +1,24 @@
+"""Enunciado:
+Crie um programa que vai ler 'vários números' e colocar em uma 'list'.
+Depois disso, mostre:
+a) 'Quantos' números foram digitados.
+b) A lista de valores, ordenada de forma 'decrescente'.
+c) Se o valor '5' foi digitado e está ou não na 'lista'.
+"""
+
+nums = []
+while True:
+    check = " "
+    nums.append(int(input("Digite um valor: ")))
+    while check not in "SN":
+        check = str(input("Deseja continuar? [S/N]")).upper().strip()[0]
+    if check == "N":
+        break
+nums.sort(reverse=True)
+print("-=" * 15 + "-")
+print(f"Você escreveu {len(nums)} elementos.")
+print(f"Os valores em ordem decrescente são {nums}")
+if 5 in nums:
+    print(f"O valor 5 foi encontrado na lista!")
+else:
+    print(f"O valor 5 não foi encontrado na lista!")
