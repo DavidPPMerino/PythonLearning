21c1940ea96c831c6c8515e51b47062fc6f87263
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-06 22:39:11 +0100

    Added Curso em Video solution: desafio083.py

--- Curso em Video/Desafios/8x/desafio083.py
+++ Curso em Video/Desafios/8x/desafio083.py
@@ -17,3 +17,20 @@ if open == close:
     print("Expressão valida")
 else:
     print("Expressão inválida")
+
+""" Resolução Curso em Video """
+expr = input('Digite a expressão: ')
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
