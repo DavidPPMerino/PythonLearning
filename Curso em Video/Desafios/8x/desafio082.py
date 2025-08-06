b557b068f62edd5f3caa3eedef90a368d9dcf7dc
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-06 15:37:39 +0100

    Solved: desafio082.py

--- Curso em Video/Desafios/8x/desafio082.py
+++ Curso em Video/Desafios/8x/desafio082.py
@@ -5,3 +5,24 @@ e os valores 'ímpares' digitados, respetivamente.
 Ao final, mostre o conteúdo das 'três listas' geradas.
 """
 
+nums = []
+while True:
+    nums.append(int(input("Escreva um valor: ")))
+    check = " "
+    while check not in "SN":
+        check = str(input("Deseja continuar? [S/N] ")).upper().strip()[0]
+    if check == "N":
+        break
+evens = []
+odds = []
+for num in nums:
+    if num % 2:
+        odds.append(num)
+    else:
+        evens.append(num)
+print("-=" * 15 + "-")
+print(f"A lista completa é {nums}")
+print(f"A lista de pares é {evens}")
+print(f"A lista de ímpares é {odds}")
+
+
