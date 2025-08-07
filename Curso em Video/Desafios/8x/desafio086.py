8d4db2fbf50f3344195e0d09483c9849cfa8fa5c
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-08 00:53:08 +0100

    Added Curso em Video solution: desafio084.py → desafio089.py

--- Curso em Video/Desafios/8x/desafio086.py
+++ Curso em Video/Desafios/8x/desafio086.py
@@ -6,13 +6,27 @@ No final, mostre a 'matriz' na tela, com a formatação correta.
 [ 4 ][ 5 ][ 6 ]
 [ 7 ][ 8 ][ 9 ]
 """
-matrix = [[], [], []]
-for col, line in enumerate(matrix):
-    for i in range(0, 3):
-        num = int(input(f"Escreva um valor para [{col}, {i}]: "))
-        matrix[col].insert(i, num)
-print("-=" * 20 + "-")
-for line in matrix:
-    for num in line:
-        print(f"[ {num} ]", end="")
+
+"V1.0"
+# matrix = [[], [], []]
+# for col, line in enumerate(matrix):
+#     for i in range(0, 3):
+#         num = int(input(f"Escreva um valor para [{col}, {i}]: "))
+#         matrix[col].insert(i, num)
+# print("-=" * 20 + "-")
+# for line in matrix:
+#     for num in line:
+#         print(f"[ {num} ]", end="")
+#     print()
+
+""" Resolução Curso em Video """
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
     print()
+    
