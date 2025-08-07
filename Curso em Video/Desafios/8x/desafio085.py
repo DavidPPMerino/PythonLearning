fa0b7908aff422a687976fa9c815cccd3108bb2d
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-07 16:35:31 +0100

    Solved: desafio085.py

--- Curso em Video/Desafios/8x/desafio085.py
+++ Curso em Video/Desafios/8x/desafio085.py
@@ -5,3 +5,17 @@ cadastre-os em uma 'lista única' que mantenha separados os valores 'pares' e
 No final, mostre os valores pares e ímpares em ordem 'crescente'.
 """
 
+nums = [[],[]]
+for _ in range(1, 8):
+    num = int(input("Escreva um valor: "))
+    if not num % 2:
+        nums[0].append(num)
+    else:
+        nums[1].append(num)
+nums[0].sort()
+nums[1].sort()
+print("-=" * 20 + "-")
+print(f"Os valores pares escritos foram: {nums[0]}")
+print(f"Os valores ímpares escritos foram: {nums[1]}")
+
+""" Resolução Curso em Video """
