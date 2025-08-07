3c6c6c4127fc8eb3f8cf7c9e68145b86a7407957
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-07 18:51:01 +0100

    Solved: desafio088.py

--- Curso em Video/Desafios/8x/desafio088.py
+++ Curso em Video/Desafios/8x/desafio088.py
@@ -3,4 +3,17 @@ Faça um programa que ajude um jogador da 'MEGA SENA' a criar 'palpites'. O
 programa vai perguntar 'quantos jogos' serão gerados e vai sortear '6 números'
 'entre 1 e 60' para cada jogo, cadastando tudo em uma 'lista composta'.
 """
-
+from time import sleep
+from random import choices
+print("--" * 20)
+print(f"{"JOGA NA MEGA SENA":^40}")
+print("--" * 20)
+qty = int(input("Quantos jogos quer que sorteie? "))
+print("-=" * 5,f"A sortear {qty} {"jogos" if qty > 1 else "jogo"}".center(20), "=-" * 5)
+sleep(2)
+for i in range(qty):
+    nums = choices(range(1, 61), k=6)
+    print(f"Jogo {i+1}: {nums}")
+    sleep(1)
+print("-=" * 5, f"< BOA SORTE >".center(20), "=-" * 5)
+print("-=" * 5, f"Jogue com moderação".center(20), "=-" * 5)
