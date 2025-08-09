0c0aae9ae0660c007bd36345302f0ff33590a94c
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-09 18:48:47 +0100

    Solved: desafio091.py

--- Curso em Video/Desafios/9x/desafio091.py
+++ Curso em Video/Desafios/9x/desafio091.py
@@ -6,3 +6,19 @@ No final, coloque esse dicionário em 'ordem' sabendo que o 'vencedor' tirou o
 """
 
 " V1.0 "
+from random import randint
+from time import sleep
+jogadores ={}
+print("Valores sorteados:")
+sleep(1)
+for i in range(1, 5):
+    jogadores[f"Jogador{i}"] = randint(1, 6)
+for k, v in jogadores.items():
+    print(f"O {k} tirou {v}")
+    sleep(1)
+print("Ranking dos jogadores:")
+sorted_jogadores = dict(sorted(jogadores.items(), key=lambda item: item[1], reverse=True))
+sleep(1)
+for i, (k, v) in enumerate(sorted_jogadores.items()):
+    print(f"{i+1}º Lugar: {k} com {v}")
+    sleep(1)
