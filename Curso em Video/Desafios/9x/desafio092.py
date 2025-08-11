b5e53d8e9f242f037786af84cd2693b78c1e3f32
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-11 18:51:04 +0100

    Solved: desafio092.py

--- Curso em Video/Desafios/9x/desafio092.py
+++ Curso em Video/Desafios/9x/desafio092.py
@@ -5,6 +5,20 @@ diferente de 'ZERO', o dicionário receberá também o 'ano de contratação' e 
 'salário'.
 Calcule e acrescente, além da 'idade', com quantos anos a pessoa vai se 
 'aposentar'.
+35 anos colaboração
 """
 
 " V1.0 "
+from datetime import date
+pessoa = {}
+
+pessoa["nome"] = input("Nome: ").strip().capitalize()
+pessoa["idade"] = date.today().year - int(input("Ano de Nascimento: "))
+pessoa["CTPS"] = input("Carteira de Trabalho (0 se não tiver): ")
+if int(pessoa["CTPS"]) != 0:
+    pessoa["contratação"] = int(input("Ano de contratação: "))
+    pessoa["salário"] = int(input("Salário: R$ "))
+    pessoa["aposentadoria"] =  pessoa["contratação"] + 35 - date.today().year + pessoa["idade"]
+print("-=" * 25 + "-")
+for k, v in pessoa.items():
+    print(f"{k} tem o valor {v}")
