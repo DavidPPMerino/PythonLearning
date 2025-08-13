cb1715dd099c20490c8618f6436565a55dee2220
KillLaptop <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-13 18:44:06 +0100

    Solved: desafio093.py

--- Curso em Video/Desafios/9x/desafio093.py
+++ Curso em Video/Desafios/9x/desafio093.py
@@ -7,3 +7,22 @@ feitos durante o campeonato.
 """
 
 " V1.0 "
+player = {}
+goals = []
+player["nome"] = input("Nome do Jogador: ") 
+games = int(input(f"Quantas jogos jogou {player["nome"]}? "))
+if games >= 1:
+    for i in range(1, (games+1)):
+        goals.append(int(input(f"Quantos golos no {i}º jogo? ")))
+player["gols"] = goals[:]
+player["total"] = sum(goals)
+print("-=" * 20 + "-")
+print(player)
+print("-=" * 20 + "-")
+for k, v in player.items():
+    print(f"O campo {k} tem o valor {v}.")
+print("-=" * 20 + "-")
+print(f"O jogador {player['nome']} jogou {len(player['gols'])} jogos.")
+for i, v in enumerate(player["gols"]):
+    print(f"  → No {i+1}º jogo, fez {f'{v} golos' if v > 1 or v == 0 else f'{v} golo'}.")
+print(f"Fez um total de {player["total"]} golos.")
