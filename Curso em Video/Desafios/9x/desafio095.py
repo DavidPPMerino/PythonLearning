5f3ed42a33725ced8c2fbc772ae716ae84e5247f
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-15 03:01:15 +0100

    Added Curso em Video solution: desafio095.py

--- Curso em Video/Desafios/9x/desafio095.py
+++ Curso em Video/Desafios/9x/desafio095.py
@@ -3,42 +3,88 @@ Aprimore o 'Desafio 093' para que ele funcione com 'vários jogadores', incluind
 um sistema de visualização de 'detalhes do aproveitamento' de cada jogador."""
 
 " V1.0 "
-players = []
-player = {}
-goals = []
+# players = []
+# player = {}
+# goals = []
+# while True:
+#     print("-" * 40)
+#     player["nome"] = input("Nome do Jogador: ").strip()
+#     games = int(input(f"Quantos jogos jogou {player["nome"]}? "))
+#     if games >= 1:
+#         for i in range(1, (games+1)):
+#             goals.append(int(input(f"Quantos golos no {i}º jogo? ")))
+#     player["gols"] = goals[:]
+#     player["total"] = sum(goals)
+#     players.append(player.copy())
+#     goals.clear()
+#     while True:
+#         check = input("Quer continuar? [S/N] ").strip().upper()
+#         if check in "SN":
+#             break
+#     if check in "N":
+#         break
+# print("-=" * 25 + "-")
+# print(f"{" Cod"} | {"Nome":^15} | {"Golos":^15} | {"Total":^7}")
+# print("-" * 51)
+# for i, p in enumerate(players):
+#     print(f"{i+1:^4} | {p["nome"]:^15} | {str(p["gols"]):^15} | {p["total"]:^7}")
+# print("-=" * 25 + "-")
+# while True:
+#     check2 = int(input("Mostrar dados de qual jogador? (999 para terminar) ")) - 1
+#     if check2 == 998:
+#         print(">>- Volte sempre -<<".center(51))
+#         break
+#     elif check2 <= (len(players) - 1):
+#         print(f"-- Levantamento do jogador {players[check2]["nome"]}:")
+#         for i, v in enumerate(players[check2]["gols"]):
+#             print(f"  → No {i+1}º jogo, fez {f'{v} golos' if v > 1 or v == 0 else f'{v} golo'}.")
+#         print("-" * 51)
+#     else:
+#         print(f"ERRO! Não existe jogador com o código {check2 + 1}! Tente novamente")
+#         print("-" * 51)
+
+""" Resolução Curso em Video """
+time = list()
+jogador = dict()
+partidas = list()
 while True:
-    print("-" * 40)
-    player["nome"] = input("Nome do Jogador: ").strip()
-    games = int(input(f"Quantos jogos jogou {player["nome"]}? "))
-    if games >= 1:
-        for i in range(1, (games+1)):
-            goals.append(int(input(f"Quantos golos no {i}º jogo? ")))
-    player["gols"] = goals[:]
-    player["total"] = sum(goals)
-    players.append(player.copy())
-    goals.clear()
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do Jogador: '))
+    tot = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f'    Quantos gols na partida {c+1}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
     while True:
-        check = input("Quer continuar? [S/N] ").strip().upper()
-        if check in "SN":
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
             break
-    if check in "N":
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
         break
-print("-=" * 25 + "-")
-print(f"{" Cod"} | {"Nome":^15} | {"Golos":^15} | {"Total":^7}")
-print("-" * 51)
-for i, p in enumerate(players):
-    print(f"{i+1:^4} | {p["nome"]:^15} | {str(p["gols"]):^15} | {p["total"]:^7}")
-print("-=" * 25 + "-")
+print('-=' * 30)
+print('cod', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
 while True:
-    check2 = int(input("Mostrar dados de qual jogador? (999 para terminar) ")) - 1
-    if check2 == 998:
-        print(">>- Volte sempre -<<".center(51))
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    if busca == 999:
         break
-    elif check2 <= (len(players) - 1):
-        print(f"-- Levantamento do jogador {players[check2]["nome"]}:")
-        for i, v in enumerate(players[check2]["gols"]):
-            print(f"  → No {i+1}º jogo, fez {f'{v} golos' if v > 1 or v == 0 else f'{v} golo'}.")
-        print("-" * 51)
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com código {busca}!')
     else:
-        print(f"ERRO! Não existe jogador com o código {check2 + 1}! Tente novamente")
-        print("-" * 51)
+        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}:')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols.')
+    print('-' * 40)
+print('<< VOLTE SEMPRE >>')
