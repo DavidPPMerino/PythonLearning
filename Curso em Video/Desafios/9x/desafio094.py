e8b138ecaccbcf6d60116b7c3433379bf21b0019
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-15 02:15:52 +0100

    Added Curso em Video solution: desafio094.py

--- Curso em Video/Desafios/9x/desafio094.py
+++ Curso em Video/Desafios/9x/desafio094.py
@@ -9,37 +9,78 @@ d) uma lista com todas as pessoas com 'idade' acima da 'média'.
 """
 
 " V1.0 "
-people = []
-person = {}
+# people = []
+# person = {}
+# while True:
+#     person["nome"] = input("Nome: ").strip()
+#     while True:
+#         person["sexo"] = input("Sexo: [M/F] ").upper().strip()
+#         if person["sexo"] in "MF":
+#             break
+#     person["idade"] = int(input("Idade: "))
+#     people.append(person.copy())
+#     while True:
+#         check = input("Quer continuar? [S/N] ").upper().strip()
+#         if check in "SN":
+#             break
+#     if check in "N":
+#         break
+# print("-=" * 20 + "-")
+# print(f"→ O grupo tem {len(people)} pessoas.")
+# average = 0
+# for p in people:
+#     average += int(p["idade"]) / len(people)
+# print(f"→ A média de idade é de {average:.0f} anos.")
+# print("→ As mulheres registadas foram: ", end=" ")
+# for p in people:
+#     if p["sexo"] in "Ff":
+#         print(p["nome"], end=" ")
+# print()
+# print("→ Lista das pessoas acima da média: ")
+# for p in people:
+#     if p["idade"] > average:
+#         for k, v in p.items():
+#             print(f"{k} = {v};", end=" ")
+#         print()    
+# print()
+# print("<< Programa terminado >>")
+
+""" Resolução Curso em Video """
+galera = list()
+pessoa = dict()
+soma = média = 0
 while True:
-    person["nome"] = input("Nome: ").strip()
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
     while True:
-        person["sexo"] = input("Sexo: [M/F] ").upper().strip()
-        if person["sexo"] in "MF":
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
             break
-    person["idade"] = int(input("Idade: "))
-    people.append(person.copy())
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
     while True:
-        check = input("Quer continuar? [S/N] ").upper().strip()
-        if check in "SN":
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
             break
-    if check in "N":
+        print('ERRO! Responda apenas S ou N')
+    if resp == 'N':
         break
-print("-=" * 20 + "-")
-print(f"→ O grupo tem {len(people)} pessoas.")
-average = 0
-for p in people:
-    average += int(p["idade"]) / len(people)
-print(f"→ A média de idade é de {average:.0f} anos.")
-print("→ As mulheres registadas foram: ", end=" ")
-for p in people:
-    if p["sexo"] in "Ff":
-        print(p["nome"], end=" ")
+print('-=' * 30)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+média = soma / len(galera)
+print(f'B) A média de idade é de {média:5.2f} anos.')
+print('C) As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p['nome']} ', end='')
 print()
-print("→ Lista das pessoas acima da média: ")
-for p in people:
-    print()
-    if p["idade"] > average:
+print('D) Lista das pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= média:
+        print('     ', end='')
         for k, v in p.items():
-            print(f"{k} = {v};", end=" ")
-print("<< Programa terminado >>")
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
