947c9fad60eaa88eea1de6172ec285f09a3c8b0f
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-16 23:10:18 +0100

    Solved desafio096.py

--- Curso em Video/Desafios/9x/desafio096.py
+++ Curso em Video/Desafios/9x/desafio096.py
@@ -4,3 +4,20 @@ de um terreno retangular (largura e comprimento) e mostre a 'área do terreno'.
 """
 
 " V1.0 "
+def area(a, b):
+    x = a * b
+    print(f'A área de um terreno de {a:.1f}x{b:.1f} é de {x:.1f}m²')
+
+def titulo(txt):
+    print('-=' * 25 + '-')
+    print(txt.center(51))
+    print('-=' * 25 + '-')
+
+
+#Programa
+
+titulo('Controle de terrenos')
+a = float(input('Largura (m): '))
+b = float(input('Comprimento (m): '))
+print('-=' * 25 + '-')
+area(a, b)
