3e9d6a1dccd6814f7200731adcd96e126aa4c30b
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-17 00:08:49 +0100

    Solved: desafio098.py

--- Curso em Video/Desafios/9x/desafio098.py
+++ Curso em Video/Desafios/9x/desafio098.py
@@ -8,3 +8,36 @@ c) Uma contagem 'personalizada'.
 """
 
 " V1.0 "
+from time import sleep
+def mostraLinha():
+    print('-=' * 25 + '-')
+
+def contador(a, b, c):
+    mostraLinha()
+    if c == 0:
+        c = 1
+    print(f'Contagem de {a} até {b} de {abs(c)} em {abs(c)}')
+    if a > b and c > 0:
+        c = -c
+    if a < b:
+        b += 1
+    elif a > b:
+        b -= 1
+
+    for i in range(a, b, c):
+        print(f'{i} ', end='', flush=True)
+        sleep(0.2)
+    print('FIM!')
+
+def titulo(txt):
+    print('-=' * 25 + '-')
+    print(txt.center(51))
+    print('-=' * 25 + '-')
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+titulo('Agora é a tua vez de personalizar a contagem!')   
+i = int(input('Início: '))
+f = int(input('Fim: '.ljust(8)))
+p = int(input('Passo: '.ljust(8)))
+contador(i, f, p)
