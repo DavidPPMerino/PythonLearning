82f88c7e02095843b61d5d607e31acb61157afc6
KillPC <21142629+DavidPPMerino@users.noreply.github.com>
2025-08-17 01:03:54 +0100

    Solved: desafio100.py

--- Curso em Video/Desafios/10x/desafio100.py
+++ Curso em Video/Desafios/10x/desafio100.py
@@ -7,3 +7,23 @@ pela função anterior.
 """
 
 " V1.0 "
+from random import randint
+from time import sleep
+
+def sorteia(a):
+    print('Sorteando 5 valores da lista:', end=' ')
+    for i in range(5):
+        a.append(randint(1, 10))
+        print(f'{a[-1]}', end=' ', flush=True)
+        sleep(0.4)
+    print('Pronto!')
+def somaPar(a):
+    soma = 0
+    for num in a:
+        if num % 2 == 0:
+            soma += num
+    print(f'Somando os valores pares de {a}, temos {soma}')
+
+numeros = []
+sorteia(numeros)
+somaPar(numeros)
